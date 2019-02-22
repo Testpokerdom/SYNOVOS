57f3ec312c001c78ef238e6706983a5b25cbf787
Testpokerdom <v.bibik@pokerdom.com>
2019-02-22 19:22:28 +0200

    New function - feel Vendors info were added

--- src/test/java/TMA_SYNC/Tests/CreatePurchaseRequisition.java
+++ src/test/java/TMA_SYNC/Tests/CreatePurchaseRequisition.java
@@ -56,6 +56,7 @@ public class CreatePurchaseRequisition {
     }
 
 
+    
     @Test
     @Description("Create PR with valid items parameters")
     //@DisplayName("Login as Valid and Existing User")
