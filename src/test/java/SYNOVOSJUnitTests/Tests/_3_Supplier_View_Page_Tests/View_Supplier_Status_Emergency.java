e1bf2e85c502c38d1a80fd60de82b90260e03de8
Testpokerdom <v.bibik@pokerdom.com>
2019-04-19 18:36:26 +0300

    View Supplier tests: were added tests for suppliers with statuses - Approved, Emergency, New Supplier - Pending Approval

--- src/test/java/SYNOVOSJUnitTests/Tests/_3_Supplier_View_Page_Tests/View_Supplier_Status_Emergency.java
+++ src/test/java/SYNOVOSJUnitTests/Tests/_3_Supplier_View_Page_Tests/View_Supplier_Status_Emergency.java
@@ -3,7 +3,7 @@ package SYNOVOSJUnitTests.Tests._3_Supplier_View_Page_Tests;
 import SYNOVOSJUnitTests.Locators.LoginPage.LoginPageLocators;
 import SYNOVOSJUnitTests.Locators.MainPage.MainPageLocators;
 import SYNOVOSJUnitTests.Locators.PurchasingPage.*;
-import SYNOVOSJUnitTests.Tests.LoginPage.LoginPageTests;
+import SYNOVOSJUnitTests.Tests._1_Create_Supplier_New_NewPendingApproval_Emergency_Approved_Reject.Create_Supplier_Status_Emergancy;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.After;
@@ -16,10 +16,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
 import static SYNOVOSJUnitTests.WebHelpers.WebHelpers.getTextFronWebElement;
+import static SYNOVOSJUnitTests.WebHelpers.WebHelpers.goToUrl;
 import static WebHelpers.WebHelpers.*;
 import static WebHelpers.WebHelpers.clickButtonIfEnable;
 
-public class View_Supplier_Status_New_Pending_Approval {
+public class View_Supplier_Status_Emergency {
     public static WebDriver driver = null;
     public static LoginPageLocators loginPageLocators = null;
     public static MainPageLocators mainPageLocators = null;
@@ -29,8 +30,7 @@ public class View_Supplier_Status_New_Pending_Approval {
     public static ApproveSupplierPage approveSupplierPage = null;
     public static SupplierDetailPage supplierDetailPage = null;
     public static EditSupplierPage editSupplierPage= null;
-    public static final Logger logger = LogManager.getLogger(LoginPageTests.class);
-    public static String supplierNumber;
+    public static Logger logger = LogManager.getLogger(Create_Supplier_Status_Emergancy.class);
 
     @Before
     public void beforEeach() {
@@ -51,41 +51,35 @@ public class View_Supplier_Status_New_Pending_Approval {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
-        logger.info("------------------------------------------------------");
         goToUrl(driver, "http://localhost:8080/sos/splashScreen.sos");
         sendTextToMultipleWebElements(loginPageLocators.fieldLogin, "VBIBIKSU", loginPageLocators.fieldPassword, "deadman11");
-        clickButton(loginPageLocators.buttonLogin);
-        clickElement(mainPageLocators.tablePurchasing);
-    }
+        clickButtonIfEnable(loginPageLocators.buttonLogin);
+        clickButtonIfEnable(mainPageLocators.tablePurchasing);
 
+    }
+/*
     @After
     public void afterEach() {
 
         driver.quit();
     }
-
+*/
     @Test
-    //@Description("Create Supplier")
-    //@DisplayName("Create Supplier")
-    public void createSupplierWithStatusNewSupplierPendingApproval(){
+    public void createSupplierEmergency(){
         clickButtonIfEnable(mainPageLocators.linkSupplier);
         selectWebElementFromDropDownList(supplierSearchCreatePage.dropdownListSiteCode, "SALES"); // 130 - AGRO_FARMA;  SALES - DEMO;
         clickButton(supplierSearchCreatePage.buttonCreate);
-        createSupplierPage.fillUserDataTableAndSave2("asd@ukr.net", "Supplier_status_APPROVED_DEMO_SALES_", "","999-999-9999", "@ukr.net");
-        clickButtonIfEnable(createSupplierPage.buttonSendForApproval);
-        sendTextToWebElement(createSupplierPage.fieldComments, "Supplier was created and sent for approval with status New Supplier - Pending Approval");
+        createSupplierPage.fillUserDataEmergencySupplier("asd@ukr.net","Supplier_Emergency_DEMO_SALES_", "123-123-1234", "@ukr.net", "Emergency supplier was created");
         clickButtonIfEnable(createSupplierPage.buttonOKpopup);
-
-        System.out.println("Supplier was created, his Number is: " + getTextFronWebElement(supplierDetailPage.fieldSupplierNumber));
-        logger.info("Supplier was created, his Number is: " + getTextFronWebElement(supplierDetailPage.fieldSupplierNumber));
+        System.out.println("Emergency Supplier was created, his number is: " + getTextFronWebElement(supplierDetailPage.fieldSupplierNumber));
+        logger.info("Emergency Supplier was created, his number is: " + getTextFronWebElement(supplierDetailPage.fieldSupplierNumber));
         logger.info("------------------------------------------------------");
-        System.out.println("Is button Send For Approval available: " + supplierDetailPage.buttonSendForApproval.isEnabled());
 
-        Assert.assertEquals("Supplier Detail", supplierDetailPage.textSupplierDetails.getText());
+        Assert.assertEquals("Supplier Detail", createSupplierPage.textSupplierDetails.getText());
         Assert.assertEquals("ASD@UKR.NET", supplierDetailPage.fieldRemitTo.getText());
         Assert.assertEquals("Yes", supplierDetailPage.fieldProvidedW9.getText());
-        Assert.assertEquals("New Supplier - Pending Approval", getTextFronWebElement(supplierDetailPage.fieldApprovalStatus)); // Approval status should be New Supplier - Pending Approval
+        Assert.assertEquals("Emergency", getTextFronWebElement(supplierDetailPage.fieldApprovalStatus)); // Approval status should be Emergency
         Assert.assertFalse(supplierDetailPage.buttonSendForApproval.isEnabled() == true);  // Button Send For Approval should be inActive!!!!!
-        //Assert.assertEquals("false", supplierDetailPage.buttonSendForApproval.isEnabled());
+
     }
 }
